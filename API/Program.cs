ed719d8d231085d516c69d5f2a01a4b37f260ff7
juangom1950 <juangom73@gmail.com>
2021-10-19 17:25:01 -0400

    Using the program_cs class to migrate and seed the data app startup

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -14,7 +13,28 @@ namespace API
         public static void Main(string[] args)
         {
             //It creates the castro server by default to run the API there
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            //"using" here is going to releaze resources when this method isn't longer in the scope
+            //It is going to dispose scope resources. It is doing the same thatn "finally" after the catch
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            //We add this logger here to be able to through our exeptions in the command prompt if there is any
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try 
+            {
+                context.Database.Migrate();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Problem migrating data");
+            }
+            // finally
+            // {
+            //     scope.Dispose();
+            // }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
