dd885da62244812447ab9682d26007f8f53d4c5e
juangom1950 <juangom73@gmail.com>
2021-10-20 00:05:51 -0400

    What's in the React template

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,35 +1,38 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
-    {   
+    {
         //We add this for dependency injection
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
         public ProductsController(StoreContext context)
         {
-            this.context = context;
-
+            _context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
+            //This is the same than below
+            return await _context.Products.ToListAsync();
 
-            return Ok(products);
+            // var products = await context.Products.ToListAsync();
+            // return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    //this is jsx
+    <div>
+      <h1>Re-Store</h1>
+    </div>
+  );
+}
+
+export default App;
