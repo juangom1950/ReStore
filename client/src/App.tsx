12a8d336b5eaa7db0e1711c1b9b42277f6b5fcd7
juangom1950 <juangom73@gmail.com>
2021-10-20 00:42:41 -0400

    Using useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
+import { isTemplateTail } from 'typescript';
 
 function App() {
+
+  const [products, setProducts] = useState([
+    //Initial state
+    {name: 'produc1', price: 100.00},
+    {name: 'produc1', price: 200.00},
+  ]);
+
+  function addProduct() {
+    setProducts(prevState => [...prevState,
+      {name:'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}
+    ])
+  }
+
   return (
     //this is jsx
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>App Product</button>
     </div>
   );
 }
