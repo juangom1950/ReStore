9199bc8c23f1f741547f9d2eb6deab17725ca7f0
juangom1950 <juangom73@gmail.com>
2021-10-21 18:12:42 -0400

    Adding cards for our individual products

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -5,7 +5,11 @@ export default function Header() {
     // Check Material UI Documantation
     // https://mui.com/components/app-bar/
     return (
-        <AppBar position='static'>
+        // Mat UI sx:  https://mui.com/system/the-sx-prop/#main-content
+        // Material UI spacing: https://mui.com/system/the-sx-prop/#spacing
+        // The `sx` prop is a shortcut for defining custom style that has access to the theme
+        // mb is margin buttom
+        <AppBar position='static' sx={{mb: 4}}>
             <Toolbar>
                 <Typography variant='h6'>
                     Re-Store
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,5 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+//import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, CardActions, Button } from "@mui/material";
 import { Product } from "../../app/models/products";
 
 interface Props {
@@ -7,13 +8,37 @@ interface Props {
 
 export default function ProductCard({product}: Props) {
     return (
-        <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.pictureUrl} />
-            </ListItemAvatar>
-            <ListItemText>
-                {product.name} - {product.price}
-            </ListItemText>
-        </ListItem>
+        // <ListItem key={product.id}>
+        //     <ListItemAvatar>
+        //         <Avatar src={product.pictureUrl} />
+        //     </ListItemAvatar>
+        //     <ListItemText>
+        //         {product.name} - {product.price}
+        //     </ListItemText>
+        // </ListItem>
+
+        // Material Ui Doc: https://mui.com/components/cards/#main-content
+        // Material UI Grid: https://mui.com/components/grid/#main-content
+        <Card>
+            <CardMedia
+                component="img"
+                height="140"
+                image="http://picsum.photos/200"
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     )
 }
