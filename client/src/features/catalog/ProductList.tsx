cc49d6b61e1306d5f215f0d72d9360d48a5e6eeb
juangom1950 <juangom73@gmail.com>
2021-10-22 13:31:52 -0400

    Styling de product card

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -11,7 +11,10 @@ export default function ProductList({products}: Props) {
         // This List is from material ui */}
         <Grid container spacing={4}>
             {products.map((product) => (
-                <Grid item xs={4} key={product.id}>
+                //For each with takes 3 spaces out of the 12 spaces of the toal with.
+                //xs is "extra small and up" take the whole with (12) and when it gets to
+                //md={3} takes, each takes 3 out of 12. https://www.youtube.com/watch?v=GYTN5JdkLSQ
+                <Grid item xs={12} md={3} key={product.id}>
                     <ProductCard product={product}/>
                 </Grid>
             ))}
