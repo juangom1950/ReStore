bc55be96838ca31cccc0c585f29bc693e32a42c3
juangom1950 <juangom73@gmail.com>
2021-10-23 09:14:21 -0400

    Setting up React Router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { ThemeProvider } from '@emotion/react';
 import { Container, CssBaseline, createTheme } from '@mui/material';
 import { useState } from 'react';
+import { Route } from 'react-router';
+import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
 import Header from './Header';
 
 function App() {
@@ -29,7 +34,11 @@ function App() {
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       {/* Container is a material ui component */}
       <Container>
-        <Catalog/>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/catalog' component={Catalog} />
+        <Route path='/catalog/:id' component={ProductDetails} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/contact' component={ContactPage} />
       </Container>
     </ThemeProvider>
   );
