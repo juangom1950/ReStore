e582195417ea2d0a33538bd98aa322af8f450d44
juangom1950 <juangom73@gmail.com>
2021-11-02 00:40:42 -0400

    Adding the remove item functionality

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,11 +1,31 @@
-import { Delete } from "@mui/icons-material";
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Add, Delete, Remove } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { useState } from "react";
+import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/api/context/StoreContext";
 import { BasketItem } from "../../app/models/basket";
 
 export default function BasketPage() {
     //
-    const {basket} = useStoreContext();
+    const {basket, setBasket, removeItem} = useStoreContext();
+    const [loading, setLoading] = useState(false);
+
+    function handleAddItem(productId: number) {
+        setLoading(true);
+        agent.Basket.addItem(productId)
+            .then((basket:any) => setBasket(basket))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }
+
+    function handleRemoveItem(productId: number, quantity = 1) {
+        setLoading(true);
+        agent.Basket.removeItem(productId, quantity)
+            .then(() => removeItem(productId, quantity))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }
 
     if (!basket) return <Typography variant='h3'>Your basket is empty</Typography>
 
@@ -18,7 +38,7 @@ export default function BasketPage() {
                 <TableRow>
                 <TableCell>Product</TableCell>
                 <TableCell align="right">Price</TableCell>
-                <TableCell align="right">Quantity</TableCell>
+                <TableCell align="center">Quantity</TableCell>
                 <TableCell align="right">Subtotal</TableCell>
                 <TableCell align="right"></TableCell>
                 </TableRow>
@@ -30,15 +50,26 @@ export default function BasketPage() {
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
                     <TableCell component="th" scope="row">
-                    {item.name}
+                        <Box display='flex' alignItems='center'>
+                            <img src={item.pictureUrl} alt={item.name} style={{height: 50, margin: 20}} />
+                            <span>{item.name}</span>
+                        </Box>
                     </TableCell>
                     <TableCell align="right">$ {(item.price / 100).toFixed(2)}</TableCell>
-                    <TableCell align="right">{item.quantity}</TableCell>
+                    <TableCell align="center">
+                        <LoadingButton loading={loading} onClick={() => handleRemoveItem(item.productId)} color='error'>
+                           <Remove/>
+                        </LoadingButton>
+                        {item.quantity}
+                        <LoadingButton loading={loading} onClick={() => handleAddItem(item.productId)} color='secondary'>
+                            <Add />
+                        </LoadingButton>
+                    </TableCell>
                     <TableCell align="right">$ {((item.price / 100) * item.quantity).toFixed(2)}</TableCell>
                     <TableCell align="right">
-                        <IconButton color='error'>
+                        <LoadingButton loading={loading} onClick={() => handleRemoveItem(item.productId, item.quantity)} color='error'>
                             <Delete />
-                        </IconButton>
+                        </LoadingButton>
                 </TableCell>
                 </TableRow>
                 ))}
