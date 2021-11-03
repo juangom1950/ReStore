e739700af77d12f2e278d08c82593689dcae66e1
juangom1950 <juangom73@gmail.com>
2021-11-03 18:13:50 -0400

    Updating product detail comp to add items to cart

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,19 +1,28 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import agent from "../../app/api/agent";
+import { useStoreContext } from "../../app/api/context/StoreContext";
 import NotFound from "../../app/api/errors/NotFound";
 import LoadingComponent from "../../app/layout/loadingComponent";
 import { Product } from "../../app/models/products";
 
 export default function ProductDetails() {
+    const {basket, setBasket, removeItem} = useStoreContext();
     const {id }= useParams<{id:string}>();
     //This is going to have an initial value of null
     const [product, setProduct] = useState<Product | any >(null);
     const [loading, setLoading] = useState(true);
+    const[quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false);
+    const item = basket?.items.find(i => i.productId === product?.id);
 
     //Typescript unknown vs any: https://stackoverflow.com/questions/51439843/unknown-vs-any
     useEffect(() => {
+
+        if (item) setQuantity(item.quantity);
+
          //axios.get(`http://localhost:5000/api/products/${id}`)
         agent.Catalog.details(parseInt(id))
             .then((response:unknown) => setProduct(response))
@@ -21,7 +30,34 @@ export default function ProductDetails() {
             //We took the response away now, because we are doing it from the interceptors
             .catch(error => console.log(error))
             .finally(() => setLoading(false));
-    }, [id])
+    }, [id, item])
+
+    function handleInputChange(event: any) {
+        if (event.target.value >= 0) {
+            setQuantity(parseInt(event.target.value));
+        }
+    }
+
+    function handleUpdateCart() {
+        setSubmitting(true);
+        //Verify if we have a quantity
+        if (!item || quantity > item.quantity) {
+            const updateQuantity = item ? quantity - item.quantity : quantity;
+            agent.Basket.addItem(product?.id!, updateQuantity)
+                .then((basket:any) => setBasket(basket))
+                .catch(error => console.log(error))
+                .finally(() => setSubmitting(false))
+        } else {
+            // This would give us the difference
+            const updatedQuantity = item.quantity - quantity;
+            //This is removing the quantity from the server
+            agent.Basket.removeItem(product?.id!, updatedQuantity)
+                //This is reducing the quantity from the state
+                .then(() => removeItem(product?.id!, updatedQuantity))
+                .catch(error => console.log(error))
+                .finally(() => setSubmitting(false))
+        }
+    }
 
     if (loading) return <LoadingComponent message='Loading Products...'/>
 
@@ -37,31 +73,59 @@ export default function ProductDetails() {
                <Divider sx={{mb: 2}} />
                <Typography variant='h5' color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
                <TableContainer>
-               <Table>
-                   <TableBody>
-                       <TableRow>
-                           <TableCell>Name</TableCell>
-                           <TableCell>{product.name}</TableCell>
-                       </TableRow>
-                       <TableRow>
-                           <TableCell>Description</TableCell>
-                           <TableCell>{product.description}</TableCell>
-                       </TableRow>
-                       <TableRow>
-                           <TableCell>Type</TableCell>
-                           <TableCell>{product.type}</TableCell>
-                       </TableRow>
-                       <TableRow>
-                           <TableCell>Brand</TableCell>
-                           <TableCell>{product.brand}</TableCell>
-                       </TableRow>
-                       <TableRow>
-                           <TableCell>Quantity</TableCell>
-                           <TableCell>{product.quantityInStock}</TableCell>
-                       </TableRow>
-                   </TableBody>
-               </Table>
-           </TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Type</TableCell>
+                                <TableCell>{product.type}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{product.brand}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity</TableCell>
+                                <TableCell>{product.quantityInStock}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+               </TableContainer>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <TextField
+                            onChange={handleInputChange}
+                            variant='outlined'
+                            type='number'
+                            label='Quantity in Cart'
+                            // This would be the full with of its container
+                            fullWidth
+                            value={quantity}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <LoadingButton
+                            disabled={item?.quantity === quantity || !item && quantity === 0}
+                            loading={submitting}
+                            onClick={handleUpdateCart}
+                            sx={{height: '55px'}}
+                            color='primary'
+                            size='large'
+                            variant='contained'
+                            fullWidth
+                        >
+                            {item ? 'Update Quantity' : 'Add to CArt'}
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
+                
            </Grid>
        </Grid>
     )
