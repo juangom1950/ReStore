842df7cd698f1b23da0f5acf2a9317850804373b
juangom1950 <juangom73@gmail.com>
2021-11-09 00:51:19 -0500

    Using async functions in redux

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -1,13 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import agent from "../../app/api/agent";
 import { Basket } from "../../app/models/basket";
 
 interface BasketState {
-    basket: Basket | null
+    basket: Basket | null;
+    status: string;
 }
 
 const initialState: BasketState = {
-    basket: null
+    basket: null,
+    status: 'idle'
 }
+
+//? makes quantity optional
+//It returns Basket and accepts an object with {productId and quantity}
+export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, quantity?: number}>(
+    'basket/addBasketItemAsync',
+    //set default quantity equals 1
+    async ({ productId, quantity = 1}): Promise<Basket | any>  => {
+        try {
+            return await agent.Basket.addItem(productId, quantity);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+)
 
 export const basketSlice = createSlice({
     name: 'basket',
@@ -28,7 +45,20 @@ export const basketSlice = createSlice({
 
             if (state.basket?.items[itemIndex].quantity === 0) state.basket.items.splice(itemIndex, 1);
         }
-    }
+    },
+    extraReducers: (builder =>  {
+        builder.addCase(addBasketItemAsync.pending, (state, action) => {
+            //console.log(action);
+            state.status = 'pendingAddItem' + action.meta.arg.productId;
+        })
+        builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+            state.basket = action.payload;
+            state.status = 'idle';
+        })
+        builder.addCase(addBasketItemAsync.rejected, (state) => {
+            state.status = 'idle';
+        })
+    })
 })
 
 export const {setBasket, removeItem} = basketSlice.actions;
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,32 +1,33 @@
 //import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { Card, CardMedia, CardContent, Typography, CardActions, Button, CardHeader, Avatar } from "@mui/material";
-import { useState } from "react";
+//import { useState } from "react";
 import { Link } from "react-router-dom";
-import agent from "../../app/api/agent";
+//import agent from "../../app/api/agent";
 //import { useStoreContext } from "../../app/api/context/StoreContext";
 import { Product } from "../../app/models/products";
-import { useAppDispatch } from "../../app/store/configureStore";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
 import { currencyFormat } from "../../app/util/util";
-import { setBasket } from "../basket/basketSlice";
+import { addBasketItemAsync } from "../basket/basketSlice";
 
 interface Props {
     product: Product
 }
 
 export default function ProductCard({product}: Props) {
-    const [loading, setLoading] = useState(false);
+    const {status} = useAppSelector(state => state.basket);
+    //const [loading, setLoading] = useState(false);
     //const {setBasket} = useStoreContext();
     const dispatch = useAppDispatch();
 
-    function handleAddItem(productId: number) {
-        setLoading(true);
-        agent.Basket.addItem(productId)
-            .then(basket => dispatch(setBasket(basket)))
-            //.then((basket:any) => setBasket(basket))
-            .catch((error: any) => console.log(error))
-            .finally(() => setLoading(false));
-    }
+    // function handleAddItem(productId: number) {
+    //     setLoading(true);
+    //     agent.Basket.addItem(productId)
+    //         .then(basket => dispatch(setBasket(basket)))
+    //         //.then((basket:any) => setBasket(basket))
+    //         .catch((error: any) => console.log(error))
+    //         .finally(() => setLoading(false));
+    // }
 
     return (
         <Card>
@@ -57,8 +58,8 @@ export default function ProductCard({product}: Props) {
             </CardContent>
             <CardActions>
                 <LoadingButton 
-                    loading={loading} 
-                    onClick={() => handleAddItem(product.id)} 
+                    loading={status.includes('pendingAddItem' + product.id)} 
+                    onClick={() => dispatch(addBasketItemAsync({productId: product.id}))} 
                     size="small">Add to cart
                 </LoadingButton>
                 <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
